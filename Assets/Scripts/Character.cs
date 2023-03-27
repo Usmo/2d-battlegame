a4cdccff15e8930b5ca2af503b0d36493638d913
Usmo <baduuadajab@hotmail.com>
2023-03-27 18:16:05 +0300

    Created Character and CharaterTests

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float healthPoints;
+    public float maxHealthPoints;
+    public float attackPoints;
+    public float startingHealthPoints;
+    public float startingAttackPoints;
+
+    public void TakeDamage(float damageAmount)
+    {
+
+    }
+    public void Attack(Character target)
+    {
+
+    }
+
+}
