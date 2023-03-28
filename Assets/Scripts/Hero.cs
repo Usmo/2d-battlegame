9d2673815a2577396acc8cbd47993a4eea127ac3
Usmo <baduuadajab@hotmail.com>
2023-03-28 20:42:48 +0300

    Created functions to Hero script

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -8,21 +8,41 @@ public class Hero : Character
 
     public void Heal()
     {
-        throw new NotImplementedException();
+        // Calculate heal amount (25% of maxHealthPoints) 
+        float healAmount = (float)(maxHealthPoints * 0.25);
+
+        // Add healAmount to healthPoints
+        healthPoints += healAmount;
+
+        // Check that addition does not add health points over maxHealthPoints limit
+        if (healthPoints > maxHealthPoints)
+        {
+            healthPoints = maxHealthPoints;
+        }
     }
 
     public void UpgradeHealth()
     {
-        throw new NotImplementedException();
+        // Calculate and increase maxHealthPoints by 20% of maxHealthPoints
+        maxHealthPoints += (float)(maxHealthPoints * 0.20);
+
+        // Calculate and increase healthPoints by 20% of healthPoints
+        healthPoints += (float)(healthPoints * 0.20);
     }
 
     public void UpgradeAttack()
     {
-        throw new NotImplementedException();
+        // Calculate and increase attackPoints by 20% of attackPoints
+        attackPoints += (float)(attackPoints * 0.20);
     }
 
     public void RespawnHero()
     {
-        throw new NotImplementedException();
+        // Set healthPoints and maxHealthPoints to startingHealthPoints
+        maxHealthPoints = startingHealthPoints;
+        healthPoints = startingHealthPoints;
+
+        // Set attackPoints to startingAttackPoints
+        attackPoints = startingAttackPoints;
     }
 }
