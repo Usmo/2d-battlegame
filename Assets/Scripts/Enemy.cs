4f284a0e3cc6bb388579b77f8a66d4117270b628
Usmo <baduuadajab@hotmail.com>
2023-03-28 21:46:31 +0300

    Added EnemyTests

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Character
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void RespawnEnemy(int defeatedEnemyCount)
     {
-        
+        throw new NotImplementedException();
     }
 }
