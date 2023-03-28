52a54bd8968ee11039ed9d4a85d135d790b40925
Usmo <baduuadajab@hotmail.com>
2023-03-28 22:16:39 +0300

    Created RespawnEnemy function to Enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,18 @@ public class Enemy : Character
 
     public void RespawnEnemy(int defeatedEnemyCount)
     {
-        throw new NotImplementedException();
+        // Set maxHealthPoints and attackPoints to starting values
+        maxHealthPoints = startingHealthPoints;
+        attackPoints = startingAttackPoints;
+
+        // Increase maxHealthPoints and attackPoints by 10 percent for each defeated enemy
+        for(int i = 0; i < defeatedEnemyCount; i++)
+        {
+            maxHealthPoints = (float)(maxHealthPoints * 1.1);
+            attackPoints = (float)(attackPoints * 1.1);
+        }
+
+        // Set healthPoints to same value as maxHealthPoints
+        healthPoints = maxHealthPoints;
     }
 }
