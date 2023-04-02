9109c9fbfe23bdfda439ed7ef340ba0ca784ea5c
Usmo <baduuadajab@hotmail.com>
2023-04-02 12:22:17 +0300

    Created battle system functionality

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -16,6 +16,10 @@ public class BattleSystem : MonoBehaviour
     }
     public TurnState turnState;
     public int defeatedEnemyCount = 0;
+
+    public GameObject playerActionCanvas;
+    public GameObject playerUpgradeCanvas;
+    public GameObject endScreenCanvas;
     
     // Start is called before the first frame update
     void Start()
@@ -31,51 +35,169 @@ public class BattleSystem : MonoBehaviour
 
     public void EnemyAttacksHero()
     {
-        throw new NotImplementedException();
+        // Enemy attcks hero
+        enemy.Attack(hero);
+
+        // End enemys turn
+        EndTurnAndMoveToNextTurn();
     }
 
     public void HeroHealSelected()
     {
-        throw new NotImplementedException();
+        // Hero heals
+        hero.Heal();
+
+        // Hide action buttons
+        HideActionButtons();
+
+        // End players turn
+        EndTurnAndMoveToNextTurn();
     }
 
     public void HeroAttackSelected()
     {
-        throw new NotImplementedException();
+        // Hero attack enemy
+        hero.Attack(enemy);
+
+        // Hide action buttons
+        HideActionButtons();
+
+        // End players turn
+        EndTurnAndMoveToNextTurn();
     }
 
     public void HeroArmorUpgradeSelected()
     {
-        throw new NotImplementedException();
+        // Upgrade heros health
+        hero.UpgradeHealth();
+
+        // Hide upgrade selection buttons
+        HideActionButtons();
+
+        // End Upgrade turn phase
+        EndTurnAndMoveToNextTurn();
     }
 
     public void HeroWeaponUpgradeSelected()
     {
-        throw new NotImplementedException();
+        // Upgrade heros attack
+        hero.UpgradeAttack();
+
+        // Hide upgrade selection buttons
+        HideActionButtons();
+
+        // End Upgrade turn phase
+        EndTurnAndMoveToNextTurn();
     }
 
     public void ShowActionButtons()
     {
-        throw new NotImplementedException();
+        // Check what turnState game is at
+        if (turnState == TurnState.PLAYER_TURN)
+        {
+            // Set PlayerActionCanvas active to show action options to player
+            playerActionCanvas.SetActive(true);
+        }
+        else if (turnState == TurnState.PLAYER_UPGRADE)
+        {
+            // Set PlayerUpgradeCanvas active to show upgrade options to player
+            playerUpgradeCanvas.SetActive(true);
+        }
     }
 
     public void HideActionButtons()
     {
-        throw new NotImplementedException();
+        // Set player action and upgrade canvases inactive
+        playerActionCanvas.SetActive(false);
+        playerUpgradeCanvas.SetActive(false);
     }
 
     public void EndTurnAndMoveToNextTurn()
     {
-        throw new NotImplementedException();
+        
+        if (hero.healthPoints == 0f)
+        {
+            // End game if hero is at zero health points
+            EndGame();
+        } 
+        else if (enemy.healthPoints == 0f)
+        {
+            // Add counter to defeated enemies if enemy is at zero health points
+            defeatedEnemyCount += 1;
+
+            // Set turn state to player upgrade if defeatedEnemyCount is multiple of three
+            if (defeatedEnemyCount % 3 == 0)
+            {
+                turnState = TurnState.PLAYER_UPGRADE;
+            }
+
+            // Respawn enemy with defeated enemy counter modifier
+            enemy.RespawnEnemy(defeatedEnemyCount);
+        } 
+        else
+        {
+            // Change turnState if both characters are alive
+            if(turnState == TurnState.PLAYER_TURN)
+            {
+                turnState = TurnState.ENEMY_TURN;
+            }
+            else if (turnState == TurnState.ENEMY_TURN)
+            {
+                turnState = TurnState.PLAYER_TURN;
+            }
+        }
+
+        // Check current turn state and define next turn
+        switch (turnState)
+        {
+            case TurnState.PLAYER_TURN:
+                // Show action options on players turn
+                ShowActionButtons();
+                break;
+
+            case TurnState.PLAYER_UPGRADE:
+                // Show upgrade options on player upgrade phase
+                ShowActionButtons();
+                break;
+                    
+            case TurnState.END:
+                break;
+
+            case TurnState.ENEMY_TURN:
+                // Enemy attacks on its turn
+                EnemyAttacksHero();
+                break;
+
+            default:
+                break;
+        }
     }
 
     public void EndGame()
     {
-        throw new NotImplementedException();
+        // Set turn state to END
+        turnState = TurnState.END;
+
+        // Activate end screen
+        endScreenCanvas.SetActive(true);
     }
 
     public void RestartGame()
     {
-        throw new NotImplementedException();
+        // Set defeated enemy counter to zero
+        defeatedEnemyCount = 0;
+
+        // Respawn both characters
+        hero.RespawnHero();
+        enemy.RespawnEnemy(defeatedEnemyCount);
+
+        // Deactivate end screen
+        endScreenCanvas.SetActive(false);
+
+        // Set turn state to player turn
+        turnState = TurnState.PLAYER_TURN;
+
+        // Show player action buttons
+        ShowActionButtons();
     }
 }
